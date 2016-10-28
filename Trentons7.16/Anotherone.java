70e312b84db4462ef56873e47f915c1060070f43
Trenton McConnell <Trenton McConnell>
2016-10-28 10:35:41 -0500

    this is another test

---
+++ Trentons7.16/Anotherone.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Anotherone here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Anotherone
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+        System.out.println("this is a different thing");
+        
+    }
+}
